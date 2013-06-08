7ccc0228a4652504ec0d5777839cfcdb825f0165
Tammer Rashdan <ttroni50@gmail.com>
2013-06-08 14:15:38 -0400

    Added x and bitmap variables, accessors, and mutators and a Player constructor

--- trycatchgit/TryCatch/src/com/ap/trycatch/Player.java
+++ trycatchgit/TryCatch/src/com/ap/trycatch/Player.java
@@ -1,5 +1,30 @@
 package com.ap.trycatch;
 
+import android.graphics.Bitmap;
+
 public class Player {
 
+	private int x;
+	private Bitmap bitmap;
+	
+	public Player (int x, Bitmap bitmap) {
+		this.x = x;
+		this.bitmap = bitmap;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public Bitmap getBitmap() {
+		return bitmap;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setBitmap(Bitmap bitmap) {
+		this.bitmap = bitmap;
+	}
 }
